b93f24bc4b740786777118cd864c73446ecc993c
452880 <a452880@452880-DOA4012400015.local>
2025-07-16 14:38:11 +0900

    Initial commit

---
+++ sidebars.js
@@ -0,0 +1,23 @@
+module.exports = {
+  tutorialSidebar: [
+    {
+      type: 'category',
+      label: '시작 가이드',
+      items: [
+        'intro/getting-started',
+        'intro/installation',
+        'intro/usage',
+      ],
+    },
+    {
+      type: 'category',
+      label: '워크플로우',
+      items: [
+        'workflow/what-is-workflow',
+        'workflow/setup',
+        'workflow/examples',
+        'workflow/faq',
+      ],
+    },
+  ],
+};
